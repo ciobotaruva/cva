a4893896adeef28fa9c8ff10add328209906be3a
Ciobotaru Valentin <Ciobotaruva@gmail.com>
2020-06-25 22:55:36 +0300

    Contact form test

--- src/Componets/Sections/Contact.js
+++ src/Componets/Sections/Contact.js
@@ -1,20 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
 
 import '../Style/Contact.css';
 
 export default function Contact() {
 
+    const [formData, setFormData] = useState({
+        'name': '',
+        'email': '',
+        'subject': '',
+        'message': ''
+    })
+
+    function handleInputChange(e) {
+        setFormData({
+            ...formData,
+            [e.currentTarget.id]: e.currentTarget.value
+        });
+    }
+
+
+
+    console.log(formData);
     return (
         <section className="contact-section" id="contact">
             <div className="contact-css">
                 <h1>Get In Touch</h1>
                 <p>Please send a message through the contact form or e-mail me at <span>ciobotaruva@gmail.com</span>, I will get back to you immediatly.</p>
-                <form>
-                    <input type='text' placeholder="YOUR NAME" />
-                    <input type='text' placeholder="E-MAIL" />
-                    <input type='text' placeholder="SUBJECT" />
-                    <textarea placeholder="YOUR TEXT HERE"></textarea>
-                    <button>Submit</button>
+                <form action="mailto:ciobotaruva@gmail.com" method="POST" enctype="text/plain">
+                    <input onChange={handleInputChange} value={formData.name} name='name' type='text' id='name' placeholder="YOUR NAME" />
+                    <input onChange={handleInputChange} value={formData.email} name='email' type='email' id='email' placeholder="E-MAIL" />
+                    <input onChange={handleInputChange} value={formData.subject} name='subject' type='text' id='subject' placeholder="SUBJECT" />
+                    <textarea onChange={handleInputChange} value={formData.message} name='message' id='message' placeholder="YOUR TEXT HERE"></textarea>
+                    <input type="submit" name="submit" />
                 </form>
             </div>
         </section>
