9c16efb666a445d6d44af858d7cd639dd44d77a4
Ciobotaru Valentin <Ciobotaruva@gmail.com>
2020-07-14 19:03:23 +0300

    Contact paget with mailto

--- src/Componets/Sections/Contact.js
+++ src/Componets/Sections/Contact.js
@@ -1,5 +1,6 @@
 import React from 'react';
 
+import { mail } from '../config';
 import '../Style/Contact.css';
 
 export default function Contact() {
@@ -9,15 +10,7 @@ export default function Contact() {
             <div className="contact-css">
                 <h1>Get In Touch</h1>
                 <p>Please send a message through the contact form or e-mail me at <span>ciobotaruva@gmail.com</span>, I will get back to you immediatly.</p>
-                <form name="contact" method="POST" data-netlify-recaptcha="true" data-netlify="true">
-                    <input type="text" name="email" placeholder="E-MAIL" />
-                    <input type='text' name="name" placeholder="FULL NAME" />
-                    <textarea name='message' placeholder="YOUR TEXT HERE"></textarea>
-                    <p className="hidden">
-                        <label>Don’t fill this out if you're human: <input name="bot-field" /></label>
-                    </p>
-                    <button type="submit">Send</button>
-                </form>
+                <a href={`mailto:${mail}`}>Email Me @</a>
             </div>
         </section>
     )
--- src/Componets/config.js
+++ src/Componets/config.js
@@ -1,9 +1,10 @@
 const aboutUrl = 'https://my-json-server.typicode.com/ciobotaruva/portfolio-api/about';
 const homeUrl = 'https://my-json-server.typicode.com/ciobotaruva/portfolio-api/home';
 const experienceUrl = 'https://my-json-server.typicode.com/ciobotaruva/portfolio-api/experience';
-
+const mail = 'ciobotaruva@gmail.com';
 export {
     aboutUrl,
     homeUrl,
     experienceUrl,
+    mail,
 }
