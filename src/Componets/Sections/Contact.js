586feb0ae9dd5e41e7aa8b6d30d504ec1095c620
Ciobotaru Valentin <Ciobotaruva@gmail.com>
2020-07-14 18:49:23 +0300

    Form test v3

--- src/Componets/Sections/Contact.js
+++ src/Componets/Sections/Contact.js
@@ -11,10 +11,10 @@ export default function Contact() {
                 <p>Please send a message through the contact form or e-mail me at <span>ciobotaruva@gmail.com</span>, I will get back to you immediatly.</p>
                 <form name="contact" method="POST" data-netlify-recaptcha="true" data-netlify="true">
                     <input type="text" name="email" placeholder="E-MAIL" />
-                    <input name='subject' type='text' placeholder="SUBJECT" />
+                    <input type='text' name="name" placeholder="FULL NAME" />
                     <textarea name='message' placeholder="YOUR TEXT HERE"></textarea>
-                    <p>
-                        <div data-netlify-recaptcha="true"></div>
+                    <p className="hidden">
+                        <label>Don’t fill this out if you're human: <input name="bot-field" /></label>
                     </p>
                     <button type="submit">Send</button>
                 </form>
